abddb871854a9271d9629bb913a11ea542b033bd
Enurean <137900552+Enurean@users.noreply.github.com>
2023-08-30 08:15:20 -0300

    Registration form

--- website/views.py
+++ website/views.py
@@ -28,3 +28,7 @@ def logout_user(request):
     logout(request)
     messages.success(request, "Logged out successfully")
     return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
