dab9fac9e7307b0be71f65f367b69735ec587005
Enurean <137900552+Enurean@users.noreply.github.com>
2023-09-01 10:17:55 -0300

    Initial release

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Customer
 
 
@@ -59,4 +59,43 @@ def customer_record(request, pk):
         return render(request, 'customer_record.html', {"customer_record":customer_record})
     else:
         messages.success(request, "You must be logged in to view that page")
+        return redirect('home')
+    
+
+def delete_customer(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Customer.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Customer deleted")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to view that page")
+        return redirect('home')
+    
+
+def add_customer(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_customer = form.save()
+                messages.success(request, "Customer added")
+                return redirect('home')
+        return render(request, 'add_customer.html', {"form":form})
+    else:
+        messages.success(request, "You must be logged in to add customers")
+        return redirect('home')
+    
+def update_customer(request, pk):
+    if request.user.is_authenticated:
+        current_customer = Customer.objects.get(id=pk)
+        # Work with updated record instance
+        form = AddRecordForm(request.POST or None, instance=current_customer)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Customer updated")
+            return redirect('home')
+        return render(request, 'update_customer.html', {"form":form})
+    else:
+        messages.success(request, "You must be logged in to update customers")
         return redirect('home')
